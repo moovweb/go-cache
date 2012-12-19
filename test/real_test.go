d62ecaab1eb3a4051c8c59febdfbb75a694d17ef
Zhigang Chen <zhigang.chen@moovweb.com>
2012-12-18 18:21:39 -0800

    add some real data and tests

---
+++ test/real_test.go
@@ -0,0 +1,100 @@
+package test
+
+import "testing"
+import "go-cache"
+import "go-cache/arc"
+import "go-cache/lru"
+import "strings"
+import "io/ioutil"
+
+func TestARC(t *testing.T) {
+	cacheSize := 100
+	countCleaned := 0
+	countAdded := 0
+
+	c := arc.NewArcCache(cacheSize)
+	data, err := ioutil.ReadFile("list.txt")
+	if err != nil {
+		t.Errorf("err: %s\n", err)
+	}
+	str := string(data)
+	lines := strings.Split(str, "\n")
+	println("# of lines", len(lines))
+
+	c.SetCleanFunc(func (obj cache.CacheObject) error {
+		countCleaned += 1
+		return nil
+	})
+	c.SetFetchFunc(func (key string) (cache.CacheObject, error) {
+		countAdded += 1
+		return key, nil
+	})
+
+	countMiss := 0
+	countAccess := len(lines)
+	for i := 0; i < countAccess; i ++ {
+		_, err := c.Get(lines[i])
+		if err == cache.CacheMiss {
+			countMiss += 1
+		}
+	}
+	println("cache hit rate:", (100*(countAccess-countMiss))/countAccess, countMiss)
+
+	c.CheckCache()
+
+	if countCleaned + cacheSize != countAdded {
+		t.Errorf("numbers of data items dont match: %d != %d + %d\n", countAdded, countCleaned, cacheSize)
+	}
+	
+	for key, obj := range(c.GetAllObjects()) {
+		if key != obj.(string) {
+			t.Errorf("key does not match the cached value")
+		}
+	}
+}
+
+func TestLRU(t *testing.T) {
+	cacheSize := 100
+	countCleaned := 0
+	countAdded := 0
+
+	c := lru.NewLRUCache(cacheSize)
+	data, err := ioutil.ReadFile("list.txt")
+	if err != nil {
+		t.Errorf("err: %s\n", err)
+	}
+	str := string(data)
+	lines := strings.Split(str, "\n")
+	println("# of lines", len(lines))
+
+	c.SetCleanFunc(func (obj cache.CacheObject) error {
+		countCleaned += 1
+		return nil
+	})
+	c.SetFetchFunc(func (key string) (cache.CacheObject, error) {
+		countAdded += 1
+		return key, nil
+	})
+
+	countMiss := 0
+	countAccess := len(lines)
+	for i := 0; i < countAccess; i ++ {
+		_, err := c.Get(lines[i])
+		if err == cache.CacheMiss {
+			countMiss += 1
+		}
+	}
+	println("cache hit rate:", (100*(countAccess-countMiss))/countAccess, countMiss)
+
+	c.CheckCache()
+
+	if countCleaned + cacheSize != countAdded {
+		t.Errorf("numbers of data items dont match: %d != %d + %d\n", countAdded, countCleaned, cacheSize)
+	}
+	
+	for key, obj := range(c.GetAllObjects()) {
+		if key != obj.(string) {
+			t.Errorf("key does not match the cached value")
+		}
+	}
+}
