d1f7bb6cfe4c487c90d748a550e0247cfffa5d97
Zhigang Chen <zhigang.chen@moovweb.com>
2012-12-27 15:23:11 -0800

    update tests

--- test/fetch_test.go
+++ test/fetch_test.go
@@ -1,7 +1,7 @@
 package test
 
 import "testing"
-//import "go-cache/arc"
+import "go-cache/arc"
 import "go-cache/lru"
 import "go-cache/base"
 import "strings"
@@ -9,17 +9,14 @@ import "io/ioutil"
 import "sync"
 //import "time"
 
-/*
-func TestARCFetch(t *testing.T) {
-	c := arc.NewSafeArcCache(cacheSize)
+func TestArcFetch(t *testing.T) {
+	c := arc.NewSafeARCache(cacheSize)
 	data, err := ioutil.ReadFile("list.txt")
 	if err != nil {
 		t.Errorf("err: %s\n", err)
 	}
 	str := string(data)
 	lines := strings.Split(str, "\n")
-
-	c.SetFetchFunc(fetch)
 
 	countAccess := len(lines)
 	countAccess = 2000
@@ -28,24 +25,26 @@ func TestARCFetch(t *testing.T) {
 		wg.Add(1)
 		go func() {
 			for i := 0; i < countAccess; i ++ {
-				c.Get(lines[i])
+				_, err := c.Get(lines[i])
+				if err != nil {
+					c.Set(lines[i], &StringObject{s:lines[i]})
+				}
 			}
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-	c.CheckCache()
 
-	for key, obj := range(c.GetAllObjects()) {
+	c.Check()
+
+	for key, obj := range(c.Collect()) {
 		if key != obj.(*StringObject).s {
 			t.Errorf("key does not match the cached value")
 		}
 	}
-	c.PrintStats()
 }
-*/
 func TestLRUFetch(t *testing.T) {
-	c := lru.NewLRUCache(cacheSize)
+	c := lru.NewSafeLRUCache(cacheSize)
 	data, err := ioutil.ReadFile("list.txt")
 	if err != nil {
 		t.Errorf("err: %s\n", err)
@@ -80,7 +79,7 @@ func TestLRUFetch(t *testing.T) {
 }
 
 func TestRandomFetch(t *testing.T) {
-	c := base.NewRRCache(cacheSize)
+	c := base.NewSafeRRCache(cacheSize)
 	data, err := ioutil.ReadFile("list.txt")
 	if err != nil {
 		t.Errorf("err: %s\n", err)
--- test/real_test.go
+++ test/real_test.go
@@ -2,7 +2,7 @@ package test
 
 import "testing"
 import "go-cache"
-//import "go-cache/arc"
+import "go-cache/arc"
 import "go-cache/lru"
 import "go-cache/base"
 import "strings"
@@ -20,61 +20,13 @@ func (o *StringObject) Size() int {
 const cacheSize = 20 * 40
 const concurrency = 20
 
-/*
+
 func TestARC(t *testing.T) {
-	countCleaned := 0
-	countAdded := 0
-
-	c := arc.NewSafeArcCache(cacheSize)
-	data, err := ioutil.ReadFile("list.txt")
-	if err != nil {
-		t.Errorf("err: %s\n", err)
-	}
-	str := string(data)
-	lines := strings.Split(str, "\n")
-
-	c.SetCleanFunc(func (obj cache.CacheObject) error {
-		countCleaned += 1
-		return nil
-	})
-	c.SetFetchFunc(func (key string) (cache.CacheObject, error) {
-		countAdded += 1
-		return &StringObject{s:key}, nil
-	})
-
-	countAccess := len(lines)
-	countAccess = 2000
-	wg := &sync.WaitGroup{}
-	for j := 0; j < concurrency; j++ {
-		wg.Add(1)
-		go func() {
-			for i := 0; i < countAccess; i ++ {
-				c.Get(lines[i])
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	c.CheckCache()
-
-	if countCleaned + cacheSize != countAdded {
-		t.Errorf("numbers of data items dont match: %d != %d + %d\n", countAdded, countCleaned, cacheSize)
-	}
-	
-	for key, obj := range(c.GetAllObjects()) {
-		if key != obj.(*StringObject).s {
-			t.Errorf("key does not match the cached value")
-		}
-	}
-	c.PrintStats()
-}
-*/
-func TestLRU(t *testing.T) {
 	countCleaned := 0
 	countAdded := 0
 	countMiss := 0
 
-	c := lru.NewLRUCache(cacheSize)
+	c := arc.NewSafeARCache(cacheSize)
 	data, err := ioutil.ReadFile("list.txt")
 	if err != nil {
 		t.Errorf("err: %s\n", err)
@@ -118,7 +70,59 @@ func TestLRU(t *testing.T) {
 			t.Errorf("key does not match the cached value")
 		}
 	}
-	println("cache hit rate:", c.GetHitRate())
+	println("cache hit rate/usage/# in cache:", c.GetHitRate(), c.GetUsage(), len(c.Collect()))
+}
+
+func TestLRU(t *testing.T) {
+	countCleaned := 0
+	countAdded := 0
+	countMiss := 0
+
+	c := lru.NewSafeLRUCache(cacheSize)
+	data, err := ioutil.ReadFile("list.txt")
+	if err != nil {
+		t.Errorf("err: %s\n", err)
+	}
+	str := string(data)
+	lines := strings.Split(str, "\n")
+
+	c.SetCleanFunc(func (obj cache.CacheObject) error {
+		countCleaned += obj.Size()
+		return nil
+	})
+	countAccess := len(lines)
+	countAccess = 2000
+	wg := &sync.WaitGroup{}
+	for j := 0; j < concurrency; j++ {
+		wg.Add(1)
+		go func() {
+			for i := 0; i < countAccess; i ++ {
+				val, err := c.Get(lines[i])
+				if err == cache.CacheMiss {
+					countAdded += len(lines[i])
+					c.Set(lines[i], &StringObject{s: lines[i]})
+					countMiss += 1
+				} else if val.(*StringObject).s != lines[i] {
+					t.Errorf("key does not match the value")
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	c.Check()
+
+	if countCleaned + c.GetUsage() != countAdded {
+		t.Errorf("numbers of data items dont match: %d != %d + %d\n", countAdded, countCleaned, c.GetUsage())
+	}
+	
+	for key, obj := range(c.Collect()) {
+		if key != obj.(*StringObject).s {
+			t.Errorf("key does not match the cached value")
+		}
+	}
+	println("cache hit rate/usage/# in cache:", c.GetHitRate(), c.GetUsage(), len(c.Collect()))
 }
 
 func TestRandom(t *testing.T) {
@@ -126,7 +130,7 @@ func TestRandom(t *testing.T) {
 	countAdded := 0
 	countMiss := 0
 
-	c := base.NewRRCache(cacheSize)
+	c := base.NewSafeRRCache(cacheSize)
 	data, err := ioutil.ReadFile("list.txt")
 	if err != nil {
 		t.Errorf("err: %s\n", err)
@@ -170,5 +174,5 @@ func TestRandom(t *testing.T) {
 			t.Errorf("key does not match the cached value")
 		}
 	}
-	println("cache hit rate:", c.GetHitRate())
+	println("cache hit rate/usage/# in cache:", c.GetHitRate(), c.GetUsage(), len(c.Collect()))
 }
