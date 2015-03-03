a1f7d4f8c0245cdac32ced47eda9d0caef46da33
Mat Byczkowski <mbyczkowski@gmail.com>
2015-03-02 17:59:27 -0800

    repro case for go-cache bug

--- arc/arc_test.go
+++ arc/arc_test.go
@@ -20,17 +20,17 @@ func TestGet(t *testing.T) {
 	countCleaned := 0
 	countAccess := 10000
 
-	c := NewARCache(cacheSize*5)
+	c := NewARCache(cacheSize * 5)
 
-	c.SetCleanFunc(func (obj cache.CacheObject) error {
+	c.SetCleanFunc(func(obj cache.CacheObject) error {
 		countCleaned += obj.Size()
 		return nil
 	})
 	rand.Seed(time.Now().Unix())
 
-	for i := 0; i < countAccess; i ++ {
-		j := rand.Intn(cacheSize*2)
-		key := "key"+strconv.Itoa(j)
+	for i := 0; i < countAccess; i++ {
+		j := rand.Intn(cacheSize * 2)
+		key := "key" + strconv.Itoa(j)
 		val, err := c.Get(key)
 
 		if err == cache.CacheMiss {
@@ -42,11 +42,11 @@ func TestGet(t *testing.T) {
 	}
 
 	c.Check()
-	if countCleaned + c.GetUsage() != countAdded {
+	if countCleaned+c.GetUsage() != countAdded {
 		t.Errorf("numbers of data items dont match: %d != %d + %d\n", countAdded, countCleaned, c.GetUsage())
 	}
-	
-	for key, obj := range(c.Collect()) {
+
+	for key, obj := range c.Collect() {
 		if key != obj.(*StringObject).s {
 			t.Errorf("key does not match the cached value")
 		}
@@ -57,4 +57,60 @@ func TestGet(t *testing.T) {
 	if c.GetUsage() != 0 {
 		t.Errorf("after reset, cache usage should be zero")
 	}
+}
+
+// Taken from MPS test code that fails
+// https://github.com/moovweb/manhattan/blob/773b5c6820ea65c43846649d9dfa01016e08cae8/targets/mps/main_test.go#L237
+// It uncovers the issue with go-cache bug PI-202
+// TODO: Redo the test, and add proper assertions. Here we just wait until the code panics
+// TODO: remove refs to private repos
+func TestReproEvictionPanic(t *testing.T) {
+	var err error
+	cacheSizeUnit := 1024 * 1024
+	c := NewSafeARCache(cacheSizeUnit)
+	s1 := string(make([]byte, cacheSizeUnit))
+	obj1 := &StringObject{s: s1}
+	s2 := string(make([]byte, cacheSizeUnit))
+	obj2 := &StringObject{s: s2}
+	s3 := string(make([]byte, cacheSizeUnit+1))
+	too_big_obj := &StringObject{s: s3}
+
+	err = c.Set("key1", too_big_obj)
+	_, err = c.Get("key1")
+	err = c.Set("key1", obj1)
+	err = c.Set("key2", obj2)
+	if err != nil {
+		println(err.Error())
+	}
+
+}
+
+// Turns out calling Get() again is having an effect on cache
+// (The cache then reblances blocks or something like that)
+// TODO: remove this test after you fix the bug and improve previous test case
+func TestEvictionPanicHackyFix(t *testing.T) {
+	var err error
+	cacheSizeUnit := 1024 * 1024
+	c := NewSafeARCache(cacheSizeUnit)
+	s1 := string(make([]byte, cacheSizeUnit))
+	obj1 := &StringObject{s: s1}
+	s2 := string(make([]byte, cacheSizeUnit))
+	obj2 := &StringObject{s: s2}
+	s3 := string(make([]byte, cacheSizeUnit+1))
+	too_big_obj := &StringObject{s: s3}
+
+	println("before 1st Set")
+	err = c.Set("key1", too_big_obj)
+	println("before 1st Get")
+	_, err = c.Get("key1")
+	println("before 2nd Set")
+	err = c.Set("key1", obj1)
+	println("before 3rd Set")
+	err = c.Set("key2", obj2)
+	if err != nil {
+		println(err.Error())
+	}
+}
+
+type TestCacheObject struct {
 }
